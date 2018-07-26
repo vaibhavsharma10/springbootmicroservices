7e5a2a89856f0139bbe5d3d7643a9fe3fa4dff81
vaibhavsharma10 <vaibhav.him@gmail.com>
2018-07-26 16:04:02 +0530

    =first commit

---
+++ src/main/java/com/example/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/helloservice2")
+	public @ResponseBody String hello() {
+		return "Hello World 2";
+	}
+}
